b5b74db0e1b590d3dce94fd8127edf08de4b8746
decoder-it <35346881+decoder-it@users.noreply.github.com>
2024-10-08 11:17:34 +0200

    Update smb_control.bat

--- smb_control.bat
+++ smb_control.bat
@@ -1,5 +1,6 @@
-@echo off
 
+@echo off
+:: https://posts.specterops.io/relay-your-heart-away-an-opsec-conscious-approach-to-445-takeover-1c9b4666c8ac
 if "%1"=="" (
     echo Usage: smb_control.bat [start^|stop]
     exit /b 1
